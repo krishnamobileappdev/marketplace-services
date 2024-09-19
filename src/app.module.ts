ce4a72960746f4cfe704c9bc955c29f7ad79070e
Krishnaraj V <68226862+krishnarajmitosis@users.noreply.github.com>
2024-09-19 22:35:10 +0530

    Configure MySQL for Railway

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'password',
-      database: 'marketplace_db',
-      autoLoadEntities: true, // Automatically load entities
-      synchronize: true, // Synchronize the schema (disable in production)
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT) || 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true, // Disable in production
     }),
   ],
   controllers: [AppController],
